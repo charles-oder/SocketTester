b3c718da2d0408504fbb1ce3bafd5a79aedfa1f4
Charles Oder <charles.oder@dtn.com>
2019-01-11 11:57:07 -0600

    added history for responses

--- src/main/kotlin/us/oder/websockettestclient/ResponseView.kt
+++ src/main/kotlin/us/oder/websockettestclient/ResponseView.kt
@@ -1,22 +1,78 @@
 package us.oder.websockettestclient
 
 import javafx.beans.property.SimpleStringProperty
-import tornadofx.View
-import tornadofx.bind
-import tornadofx.textarea
-import tornadofx.vbox
+import javafx.scene.control.Button
+import tornadofx.*
 
 class ResponseView: View() {
 
     private val responseMessageObservable = SimpleStringProperty()
+    private val firstButton = Button("First")
+    private val prevButton = Button("<")
+    private val nextButton = Button(">")
+    private val lastButton = Button("Last")
+    private var responseArray: Array<String> = emptyArray()
+    private var currentResponseIndex = 0
 
     fun addResponseMessage(message: String?) {
-        responseMessageObservable.value = message
+        if (message == null) {
+            return
+        }
+        var isViewingLatest = currentResponseIndex >= responseArray.lastIndex
+        responseArray += message
+
+        if (isViewingLatest) {
+            currentResponseIndex = responseArray.lastIndex
+            responseMessageObservable.value = message
+        }
+
+        updateButtons()
     }
 
     override val root = vbox {
         textarea {
             prefHeight = 200.0
         }.bind(responseMessageObservable)
+        hbox {
+            this += firstButton
+            this += prevButton
+            this += nextButton
+            this += lastButton
+        }
     }
+
+    fun updateButtons() {
+        firstButton.isDisable = responseArray.size < 2
+        prevButton.isDisable = responseArray.size < 2 || currentResponseIndex <= 0
+        nextButton.isDisable = responseArray.size < 2 || currentResponseIndex >= responseArray.lastIndex
+        lastButton.isDisable = responseArray.size < 2
+    }
+
+    init {
+        updateButtons()
+        firstButton.setOnAction {
+            currentResponseIndex = 0
+            responseMessageObservable.value = responseArray[currentResponseIndex]
+            updateButtons()
+        }
+        prevButton.setOnAction {
+            currentResponseIndex -= 1
+            responseMessageObservable.value = responseArray[currentResponseIndex]
+            updateButtons()
+        }
+
+        nextButton.setOnAction {
+            currentResponseIndex += 1
+            responseMessageObservable.value = responseArray[currentResponseIndex]
+            updateButtons()
+        }
+        lastButton.setOnAction {
+            currentResponseIndex = responseArray.lastIndex
+            responseMessageObservable.value = responseArray[currentResponseIndex]
+            updateButtons()
+        }
+
+
+    }
+
 }
